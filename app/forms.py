02201c07ea3b44347718214069079b578a058bf2
Steven Adetomiwa <s.adetomiwa@icloud.com>
2023-03-22 13:39:06 -0500

    restart after lunch

--- app/forms.py
+++ app/forms.py
@@ -2,14 +2,18 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, EmailField
 from wtforms.validators import InputRequired, EqualTo
 
+
 class SignUpForm(FlaskForm):
     first_name = StringField('First Name', validators=[InputRequired()])
     last_name = StringField('Last Name', validators=[InputRequired()])
     username = StringField('Username', validators=[InputRequired()])
-    email = StringField('Email', validators=[InputRequired()])
-    password = StringField('Password', validators=[InputRequired()])
-    confirm_pass = StringField('Confirm Password', validators=[InputRequired(), EqualTo('password')])
+    email = EmailField('Email', validators=[InputRequired()])
+    password = PasswordField('Password', validators=[InputRequired()])
+    confirm_pass = PasswordField('Confirm Password', validators=[InputRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
 
 
-
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[InputRequired()])
+    password = PasswordField('Password', validators=[InputRequired()])
+    submit = SubmitField('Log In')
--- app/routes.py
+++ app/routes.py
@@ -1,18 +1,20 @@
 from app import app
 from flask import render_template, redirect, url_for, flash
 from fake_data import posts
-from app.forms import SignUpForm
+from app.forms import SignUpForm, LoginForm
 
-
-
-@app.route("/")
+@app.route('/')
 def index():
-    return render_template('index.html', posts = posts, logged_in = False)
+    return render_template('index.html', posts=posts, logged_in=False)
 
-@app.route("/signup", methods = ["GET", "POST"])
+
+@app.route('/signup', methods=["GET", "POST"])
 def signup():
+    # Create an instance of the form (in the context of the current request)
     form = SignUpForm()
+    # Check if the form was submitted and that all of the fields are valid
     if form.validate_on_submit():
+        # If so, get the data from the form fields
         print('Hooray our form was validated!!')
         first_name = form.first_name.data
         last_name = form.last_name.data
@@ -20,11 +22,25 @@ def signup():
         username = form.username.data
         password = form.password.data
         print(first_name, last_name, email, username, password)
-        flash(f"Thank you {first_name}, for signing up!")
-        return redirect (url_for ('index'))
-    return render_template('signup.html', form = form)
+        flash(f"Thank you {first_name} for signing up!", "success")
+        return redirect(url_for('index'))
+    return render_template('signup.html', form=form)
 
 
-@app.route('/login')
+@app.route('/login', methods=["GET", "POST"])
 def login():
-    return render_template('login.html')
+    form = LoginForm()
+    if form.validate_on_submit():
+        print('Form Validated :)')
+        username = form.username.data
+        password = form.password.data
+        print(username, password)
+        # TODO: Check if there is a user with username and that password
+        # Fake an invalid log in
+        if password == 'abc':
+            flash('Invalid username and/or password. Please try again', 'danger')
+            return redirect(url_for('login'))
+
+        flash(f'You have successfully logged in as {username}', 'success')
+        return redirect(url_for('index'))
+    return render_template('login.html', form=form)
